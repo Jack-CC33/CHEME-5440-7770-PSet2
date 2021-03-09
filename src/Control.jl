e2732ad32782dba962a751e35cc39472fd86b62b
Jack-CC33 <79158093+Jack-CC33@users.noreply.github.com>
2021-03-08 23:10:34 -0500

    Using Moon type f_I function instead of Hill-Langmuir fn

--- src/Control.jl
+++ src/Control.jl
@@ -17,7 +17,7 @@ function calculate_transcriptional_control_array(t::Float64,x::Array{Float64,1},
     n = problem["inducer_cooperativity_parameter"]
 
     # TODO: write u-varible function here 
-    f_I = σ70^n / (KD + σ70^n)
+    f_I = σ70^n / (KD^n + σ70^n)
     Z = 1 + exp(-E1/(R*T_K)) + f_I * exp(-E2/(R*T_K))
     u_variable = ( exp(-E1/(R*T_K)) + f_I * exp(-E2/(R*T_K)) ) / Z
 
